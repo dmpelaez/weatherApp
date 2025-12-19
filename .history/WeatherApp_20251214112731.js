c5afe7bddfb99582e6a5fb9e16d8a73be72fa32c
delmarpelaez <dmpelaez19gmail.com>
2025-12-19 18:26:24 +0800

    Remove .history folder from repo

--- .history/WeatherApp_20251214112731.js
+++
@@ -1,9 +0,0 @@
-const APIkey = '84c3820830b89e9c5cca040b57bbbe7a';
-const APIurl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=switzerland';
-
-
-async function weatherApp() {
-  const response = await fetch(APIurl + `&appid=${APIkey}`);
-  const data = await response.json();
-  console.log(data);
-}
